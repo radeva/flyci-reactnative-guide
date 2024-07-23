42f3e2e620d6e07d0df83499d8707187afc90342
radeva <vesita@gmail.com>
2024-07-23 14:44:27 +0300

    add a failing test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -13,5 +13,6 @@ import { it } from '@jest/globals';
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  const app = renderer.create(<App />);
+  expect(app).toMatchSnapshot();
 });
